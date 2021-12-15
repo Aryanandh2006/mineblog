456ab6f0afd5a0031033615f08acb6dd70dfd815
Aryanandh2006 <85823824+Aryanandh2006@users.noreply.github.com>
2021-12-15 21:17:41 +0530

    Update models.py

--- ablog/models.py
+++ ablog/models.py
@@ -11,7 +11,7 @@ from ckeditor.fields import RichTextField
 
 class post(models.Model):
     title = models.CharField(max_length=255)
-    header_image = models.CharField(null=True, blank=True, max_length="2038")
+    header_image = models.CharField(max_length="2038", null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
     #body = models.TextField()
